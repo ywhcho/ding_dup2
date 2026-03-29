80697adec39f3db19c619e32be23956162991eb2
bumjoo <168239968+ywhcho@users.noreply.github.com>
2026-03-29 20:40:07 +0900

    Update config/urls.py to include new URL patterns.

--- config/urls.py
+++ config/urls.py
@@ -1,5 +1,10 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
+from min_dup import views as min_dup_views
 
 urlpatterns = [
-    # Add URL patterns here
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('about/', min_dup_views.about, name='about'),
+    path('', include('min_dup.urls')),
 ]
