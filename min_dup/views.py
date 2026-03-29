ffc5112c381cd60eb85c4dce4c731a7ecf2ec5a4
bumjoo <168239968+ywhcho@users.noreply.github.com>
2026-03-29 21:05:18 +0900

    cyDup_view ed

--- min_dup/views.py
+++ min_dup/views.py
@@ -1,1 +1,11 @@
-# Add your views here
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+
+
+@login_required
+def index(request):
+    return render(request, "min_dup/index.html")
+
+
+def about(request):
+    return render(request, "min_dup/about.html")
